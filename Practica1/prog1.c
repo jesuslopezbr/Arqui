f2f73e2ed53fdcd31d31ebb392481d020aff2dc6
100346250 <100346250@alumnos.uc3m.es>
2018-02-08 10:31:34 +0100

    Practica1

---
+++ Practica1/prog1.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+  double num, result;
+  printf("Escribe un numero positivo para calcular su raiz cuadrada: \n");
+  scanf("%lf",&num);
+  result = sqrt(num);
+  printf("La raiz cuadrada de %.2lf es %.2lf\n",num,result );
+}
