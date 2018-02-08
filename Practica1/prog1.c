b1b352dc843cf268e403ffb87ea486d59b036fa4
100346250 <100346250@alumnos.uc3m.es>
2018-02-08 10:42:05 +0100

    Practica1

--- Practica1/prog1.c
+++ Practica1/prog1.c
@@ -6,6 +6,13 @@ int main(){
   double num, result;
   printf("Escribe un numero positivo para calcular su raiz cuadrada: \n");
   scanf("%lf",&num);
-  result = sqrt(num);
-  printf("La raiz cuadrada de %.2lf es %.2lf\n",num,result );
+  if(num < 0 ){
+    fprintf(stderr, "Negative number\n" );
+    exit(-1);
+  }else{
+    result = sqrt(num);
+    printf("La raiz cuadrada de %.2lf es %.2lf\n",num,result );
+    return 0;
+  }
+
 }
