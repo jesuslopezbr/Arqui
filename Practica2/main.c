eb06a69ffd82c9b30c52b7f80954c262decbf80f
100346250 <100346250@alumnos.uc3m.es>
2018-02-15 16:16:05 +0100

    Date

--- Practica2/main.c
+++ Practica2/main.c
@@ -1,6 +1,10 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <signal.h>
+#include <sys/wait.h>
+
+int go = 0;
 
 void usage(int argc){
   if(argc != 2){
@@ -22,8 +26,29 @@ void menu(){
   printf("\n ExOS> ");
 }
 
+void sigint_handler(int signo){
+  go = 1;
+}
+
 void clock_activation(){
-  execlp("/bin/date", "date -u",NULL);
+  int time_type;
+  pid_t app;
+    do{
+      while(go == 0);
+    app = fork();
+
+    if(time_type == 0){
+      if(app == 0)
+        execlp("/bin/date", "date",NULL);
+      time_type = 1;
+    }else{
+      if(app == 0)
+        execlp("/bin/date", "date","-u",NULL);
+      time_type = 0;
+    }
+    go = 0;
+  }while (1);
+
 
 }
 
@@ -31,24 +56,36 @@ int main(int argc, char *argv[]){
 
   usage(argc);
 
+ if(signal(SIGINT, sigint_handler) == SIG_ERR)
+    printf("\nError Catching signal\n");
+
+  pid_t pid_date[100];
   int choice;
   int exit = 1;
   int result = 0;
+  int i = 0;
 
   do{
     do{
-
       menu();
 
       result = scanf("%d", &choice);
       if(result == 0)
         while(fgetc(stdin) != '\n');
+
     }while(result <= 0);
 
     switch (choice) {
       case 0:
         printf("Exit\n");
         exit = 0;
+        while(pid_date[i] == 0){
+          if(pid_date != 0){
+            kill(pid_date, SIGKILL);
+          i++;
+        }
+        }
+        wait(NULL);
         break;
       case 1:
         printf("Editing user data\n");
@@ -57,11 +94,20 @@ int main(int argc, char *argv[]){
         printf("Monitoring file system\n");
         break;
       case 3:
+        pid_date[i] = fork();
         printf("Clock activated\n");
-        clock_activation();
+        if(pid_date[i] == 0)
+          clock_activation();
+        i++;
         break;
       case 4:
         printf("Clocks stopped\n");
+        i = 0;
+        while(pid_date[i] == 0){
+          if(pid_date[i] != 0){
+            kill(pid_date[i], SIGKILL);
+          i++;
+        }
         break;
       case 5:
         printf("Process monitoring\n");
