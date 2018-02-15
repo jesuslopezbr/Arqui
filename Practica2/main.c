e44d7d0ba1af953abfc975d3faae2eea421a5d4b
Jesús López <jesuslopez@wifi-218-138.uc3m.es>
2018-02-15 15:26:54 +0100

    backup1

--- Practica2/main.c
+++ Practica2/main.c
@@ -5,36 +5,94 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(int argc, char *argv[]){
+#define MAXLINE 200
 
-  pid_t pid_editor;
+int main(int argc, char *argv[])
+{
+    int n, fd[2];
+    char line1[MAXLINE];
+    char line2[MAXLINE];
+    pid_t pid_backup;
 
-  pid_editor = fork();
-
-  if(pid_editor < 0)
-  {
-    fprintf(stderr, "Editor_Fork Failed\n");
-    exit(-1);
-  }
-
-  if(pid_editor == 0)
-  {
-    //HIJO
-    int ret;
-
-    ret = execl("/usr/bin/gedit", "gedit", NULL);
-
-    if (ret == -1)
+    if (pipe(fd) < 0)
     {
-      perror("execl");
+      fprintf(stderr, "\nError in pipe creation\n\n");
+      exit(-1);
     }
-  }
-  else
-  {
-    //PADRE
-    wait(NULL);
-    printf("\nHijo Completo\n");
-    exit(0);
-  }
+    pid_backup = fork();
 
+    if(pid_backup < 0)
+    {
+      fprintf(stderr, "\nEditor_Fork Failed\n\n");
+      exit(-1);
+    }
+
+    if(pid_backup == 0)
+    {
+      //HIJO
+      close(fd[1]);
+      n = read(fd[0], line1, MAXLINE);
+      if(n < 0)
+      {
+        fprintf(stderr, "\nEditor_Fork Failed\n\n");
+        exit(-1);
+      }
+
+      write(STDOUT_FILENO, line1, n);
+
+      FILE* f_bkp = fopen("users.bkp", "wt");
+      fputs(line1, f_bkp);
+      fclose(f_bkp);
+    }
+    else
+    {
+      //PADRE
+      FILE* fb = fopen("users.data", "r");
+      if(fb == NULL)
+      {
+        printf("\nError en la apertura del archivo\n\n");
+      }
+      else
+      {
+        line2 = fgets(fb);
+      }
+      fclose(fb);
+
+      close(fd[0]);
+      write(fd[1], line2);
+    }
+}
+
+void editor()
+{
+
+    pid_t pid_editor;
+
+    pid_editor = fork();
+
+    if(pid_editor < 0)
+    {
+      fprintf(stderr, "Editor_Fork Failed\n");
+      exit(-1);
+    }
+
+    if(pid_editor == 0)
+    {
+      //HIJO
+      int ret;
+
+      ret = execl("/usr/bin/gedit", "gedit", NULL);
+
+      if (ret == -1)
+      {
+        perror("execl");
+      }
+    }
+    else
+    {
+      //PADRE
+      wait(NULL);
+      printf("\nHijo Completo\n");
+      exit(0);
+    }
 }
