0a7df46c0374980e432c18076d22dc276e3f81ce
Jesús López <jesuslopez@MacBook-Pro-de-Jesus.local>
2018-02-28 15:37:05 +0100

    Pract2 final

--- Practica2/main.c
+++ Practica2/main.c
@@ -1,17 +1,18 @@
 #include <stdio.h>  //Estandar input outut(printf, scanf y fprintf)
 #include <stdlib.h> //Estandar library(exit y malloc)
 #include <sys/types.h> //Para las funciones fork, open y wait, y el tipo pid_t
-#include <sys/stat.h>  //Para open
-#include <sys/wait.h>	//Para wait
+#include <sys/stat.h>  //Para open()
+#include <sys/wait.h>	//Para wait()
 #include <fcntl.h>  //Para open
 #include <unistd.h> //Para fork,close, execl, dup2 y sleep
 #include <string.h>  //Para la funcion strerror
 #include <errno.h>  //Para la variable errno
-#include <signal.h>
+#include <signal.h> //Para manejar señales
 
 
-int go = 0;
+int go = 0; //Variable global para controlar la señal CTRL-C
 
+//Uso para la ejecución
 void usage(int argc){
   if(argc != 2){
     fprintf(stderr, "Usage: <monitoring_time_interval>\n" );
@@ -19,6 +20,7 @@ void usage(int argc){
   }
 }
 
+//Opciones
 void menu(){
   printf("\nExtended Operating System Functions: \n");
   printf("[0] Exit - Ordered termination\n");
@@ -32,32 +34,31 @@ void menu(){
   printf("\n ExOS> ");
 }
 
-
+//Manejador de señales
 void sigint_handler(int signo){
-  go = 1;//Si recibe señal cambia variable para imprimir hora
+  go = 1; //Si recibe señal cambia variable para imprimir hora
 }
 
-/*Si recibe señal, crea nieto y imprime UTC o CET dependiendo
-de time_type*/
+//Si recibe señal, crea nieto e imprime UTC o CET dependiendo de time_type
 void clock_activation(){
   int time_type = 0, ret = 0;
   pid_t pid_time;
 
-    do{
+  do{
       go = 0;
 
       while(go == 0);
-    pid_time = fork();
+    pid_time = fork(); //Creación del nieto
 
     if(time_type == 0){
       if(pid_time == 0)
-        ret = execlp("/bin/date", "date",NULL);
+        ret = execlp("/bin/date", "date", NULL); //Imprime la fecha CET
       if (ret == -1)
         perror("execl");
       time_type = 1;
     }else{
       if(pid_time == 0)
-        ret = execlp("/bin/date", "date","-u",NULL);
+        ret = execlp("/bin/date", "date", "-u", NULL); //Imprime la fecha UTC
       if (ret == -1)
         perror("execl");
       time_type = 0;
@@ -65,11 +66,12 @@ void clock_activation(){
   }while (1);
 }
 
+//Editor -> Creamos el archivo "users.data"
 void editor()
 {
   int ret;
 
-  ret = execl("/usr/bin/gedit", "gedit", NULL);
+  ret = execl("/usr/bin/gedit", "gedit", NULL); //Abre el editor gedit
 
   if (ret == -1)
     perror("execl");
@@ -77,32 +79,33 @@ void editor()
   exit(0);
 }
 
-void child_bkp(int fd[2]){
-  //HIJO
+//Backup
+void child_bkp(int fd[2]){ //Hijo
+
   int n;
   char line1;
-  FILE* f_bkp = fopen(".users.bkp", "w");
-  close(fd[1]);
+  FILE* f_bkp = fopen(".users.bkp", "w"); //Creamos el archivo oculto para escribir
+  close(fd[1]); //Cerramos el descriptor de fichero de escritura
 
-    while((n = read(fd[0],&line1, sizeof(char))) != 0){
+    while((n = read(fd[0], &line1, sizeof(char))) != 0){ //Leemos caracter a caracter
 
-      fwrite(&line1, sizeof(char), 1, f_bkp);
+      fwrite(&line1, sizeof(char), 1, f_bkp); //Escribimos en .users.bkp caracter a caracter
       if(n < 0)
       {
         fprintf(stderr, "\nSon: Read_Pipe Failed\n\n");
         exit(-1);
       }
     }
-  fclose(f_bkp);
-  close(fd[0]);
+  fclose(f_bkp); //Cerramos el fichero
+  close(fd[0]); //Cerramos el descriptor de fichero de lectura
   printf("Closed\n");
   exit(0);
 }
 
-void father_bkp(int fd[2]){
-  //PADRE
+void father_bkp(int fd[2]){ //Padre
+
   char *line2 = NULL;
-  FILE* fb = fopen("users.data", "rb");
+  FILE* fb = fopen("users.data", "rb"); //Abrimos el archivo para leer
 
   if(fb != NULL)
   {
@@ -118,40 +121,42 @@ void father_bkp(int fd[2]){
     line2 = malloc (length);
     if(line2 != NULL)
     {
-      int fr = fread (line2, 1, length, fb);
+      int fr = fread (line2, 1, length, fb); //Copiamos de principio a fin el archivo users.data en line2
       if(fr < 0)
       {
         fprintf(stderr, "\nFather: F_Read Failed\n\n");
         exit(-1);
       }
     }
-    fclose (fb);
+    fclose (fb); //Cerramos el fichero
 
-    close(fd[0]);
-    write(fd[1], line2, length);
-    close(fd[1]);
-    free(line2);
+    close(fd[0]); //Cerramos el descriptor de fichero de lectura
+    write(fd[1], line2, length); //Escribimos en el pipe -> line2
+    close(fd[1]); //Cerramos el descriptor de fichero de escritura
+    free(line2); //Liberamos memoria dinámica
   }
   else
     perror("\nFather: Error opening file\n\n");
 }
 
+//Monitor sistema de ficheros
 void file_logger(){
 
   int log;
   int ret;
-  log = open("filesys.log", O_WRONLY|O_CREAT,S_IRUSR|S_IWUSR);
+  log = open("filesys.log", O_WRONLY|O_CREAT,S_IRUSR|S_IWUSR); //Creamos el archivo
   if(log == -1)
     perror("\nError creating file\n");
-  dup2(log,1);
-  ret = execl("/bin/ls","ls","-a",NULL);
-  close(log);
+  dup2(log,1); //Redirigimos la salida del comando "ls" al archivo "filesys.log"
+  ret = execl("/bin/ls","ls","-a",NULL); //"-a" para mostrar todos los archivos, ocultos incluidos
+  close(log); //Cerramos el fichero
   if(ret < 0)
     perror("execl");
 
   exit(0);
 }
 
+//Monitor procesos
 void proc_monitor(int n)
 {
   int ret;
@@ -160,33 +165,32 @@ void proc_monitor(int n)
 
   while(1)
   {
-    pid_ps = fork();
-    if(pid_ps == 0){
-      fd = open("process.log",O_WRONLY|O_CREAT,S_IRUSR|S_IWUSR);
+    pid_ps = fork(); //Creación del proceso hijo
+    if(pid_ps == 0){ //Hijo
+      fd = open("process.log",O_WRONLY|O_CREAT,S_IRUSR|S_IWUSR); //Creamos el archivo
       if(fd == -1)
       {
         printf("%s",strerror(errno));
       }
-      //Hijo ejecuta ps y exec
-      //Redireccionar la salida estandar al fichero abierto
-  	  dup2(fd,1);//Para que en vez de imprimir por pantalla, escriba en el fichero
-      close(fd);  //Cerramos el fichero en el hijo
-      ret = execl("/bin/ps","ps","r",NULL);
+  	  dup2(fd,1); //Redirigimos la salida del comando "ps" al archivo "process.log"
+      close(fd); //Cerramos el fichero
+      ret = execl("/bin/ps","ps","r",NULL); //"-r" para mostrar los procesos activos
       if(ret < 0)
       {
     	  perror("execl");
       }
     }
     else
-    sleep(n);
+    sleep(n); //Esperar n segundos para volver a hacer el backup
   }
 }
 
+//MAIN
 int main(int argc, char *argv[]){
 
   usage(argc);
 
- if(signal(SIGINT, sigint_handler) == SIG_ERR)
+ if(signal(SIGINT, sigint_handler) == SIG_ERR) //Error en CTRL-C
     printf("\nError Catching signal\n");
 
   pid_t pid_date[50], pid_file, pid_backup, pid_editor, pid_proc;
@@ -199,22 +203,23 @@ int main(int argc, char *argv[]){
     do{
       menu();
 
-      result = scanf("%d", &choice);
+      result = scanf("%d", &choice); //Esperar a opción que elija el usuario
       if(result == 0)
         while(fgetc(stdin) != '\n');
 
     }while(result <= 0);
 
     switch (choice) {
-      case 0:
+      case 0: //EXIT
         ex = 0;
         i--;
+        //Mandamos una señal SIGKILL a todos los procesos para matarlos al cerrar el programa
         for(;i>=0;i--){
           kill(pid_date[i],SIGKILL);
           printf("%d reporting: Terminating\n", pid_date[i]);
         }
-        if(file_stat == 1){
-          check = waitpid(pid_file, &status, WNOHANG);
+        if(file_stat == 1){ //Para saber si se ha activado el proceso
+          check = waitpid(pid_file, &status, WNOHANG); //waitpid() -> Para saber si el proceso está vivo
           if(check == 0){
             kill(pid_file,SIGKILL);
             printf("%d reporting: Terminating\n", pid_file);
@@ -243,46 +248,46 @@ int main(int argc, char *argv[]){
         }
         printf("Main process reporting: Terminating\n");
         break;
-      case 1:
+      case 1: //EDITOR
         printf("\nEditing user data\n");
-        pid_editor = fork();
-        editor_stat = 1;
+        pid_editor = fork(); //Creación del hijo
+        editor_stat = 1; //Proceso editor activado o creado
         if(pid_editor < 0)
         {
           fprintf(stderr, "\nEditor_Fork Failed\n");
           exit(-1);
         }
 
-        if(pid_editor == 0)
+        if(pid_editor == 0) //Hijo
           editor();
 
         break;
-      case 2:
+      case 2: //MONITOR FILE SYSTEM
         printf("\nMonitoring file system\n");
-        pid_file = fork();
-        file_stat = 1;
+        pid_file = fork(); //Creación del hijo
+        file_stat = 1; //Proceso file system activado o creado
         if(pid_file < 0)
         {
           fprintf(stderr, "\nFile_Fork Failed\n");
           exit(-1);
         }
-        if(pid_file == 0)
+        if(pid_file == 0) //Hijo
           file_logger();
 
         break;
-      case 3:
+      case 3: //CLOCK ACTIVATION
 
         if(i<0)
           i=0;
-        pid_date[i] = fork();
+        pid_date[i] = fork(); //Creación de array de procesos hijos
         if(pid_date[i] < 0)
         {
           fprintf(stderr, "\nDate_Fork Failed\n");
           exit(-1);
         }
-        if(pid_date[i] == 0)
+        if(pid_date[i] == 0) //Hijo
           clock_activation();
-        else{
+        else{ //Padre
           #ifdef DEBUG
             if(pid_date[i]!= 0){
               printf("%d. ", i+1);
@@ -294,9 +299,10 @@ int main(int argc, char *argv[]){
           i++;
         }
         break;
-      case 4:
+      case 4: //CLOCK STOP
         i--;
         j = i;
+        //Matar todos los procesos activados de reloj
         for(;i>=0;i--){
           #ifdef DEBUG
             printf("%d. ", i+1);
@@ -307,27 +313,27 @@ int main(int argc, char *argv[]){
         printf("\n%d Clocks stopped\n", j+1);
         i=-1;
         break;
-      case 5:
+      case 5: //PROCESS MONITOR ACTIVATION
         if(monitor_stat == 0){
           printf("\nProcess monitoring\n");
-          n = atoi(argv[1]);
+          n = atoi(argv[1]); //Argumento de ejecución "n"
 
           if((pid_proc = fork()) < 0)
           {
             fprintf(stderr, "\nProc_Monitor_Fork Failed\n");
             exit(-1);
           }
-          if(pid_proc == 0)
+          if(pid_proc == 0) //Hijo
           {
             proc_monitor(n);
-          }else{
+          }else{ //Padre
             monitor_stat = 1;
           }
           if(monitor_stat == 1)
             printf("\nProcess monitor already activated\n");
         }
         break;
-      case 6:
+      case 6: //AUTOMATIC BACK UP ACTIVATION
         printf("\nBacking up\n");
 
         if (pipe(fd) < 0)
@@ -335,8 +341,8 @@ int main(int argc, char *argv[]){
           fprintf(stderr, "\nFather: Error in pipe creation\n\n");
           exit(-1);
         }
-        pid_backup = fork();
-        backup_stat = 1;
+        pid_backup = fork(); //Creación del hijo
+        backup_stat = 1; //Proceso backup activado o creado
 
         if(pid_backup < 0)
         {
@@ -344,9 +350,9 @@ int main(int argc, char *argv[]){
           exit(-1);
         }
 
-        if(pid_backup == 0)
+        if(pid_backup == 0) //Hijo
           child_bkp(fd);
-        else
+        else //Padre
           father_bkp(fd);
         break;
       case 7:
@@ -354,6 +360,7 @@ int main(int argc, char *argv[]){
         result = 0;
         j=i;
 
+        //Comprobar y listar los procesos activos sólo si estan creados o activados
         for(; j > 0; j--){
           printf("%d reporting: Alive\n", pid_date[j-1]);
           result = 1;
@@ -391,10 +398,10 @@ int main(int argc, char *argv[]){
 
 
         break;
-      default:
+      default: //DEFAULT
         printf("Not valid\n");
         choice = 7;
         break;
     }
-  }while(ex == 1);
+  }while(ex == 1); //Repetir hasta que se haya pulsado 0 "EXIT"
 }
