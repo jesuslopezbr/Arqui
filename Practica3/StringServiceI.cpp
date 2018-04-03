5cf1af9e62e8c2740182f16edce6eda12a5c8a7a
100346250 <100346250@alumnos.uc3m.es>
2018-04-03 23:21:14 +0200

    Practica 3

--- Practica3/StringServiceI.cpp
+++ Practica3/StringServiceI.cpp
@@ -5,16 +5,16 @@
 UC3M::StringServiceI::stringSize(const ::std::string& s,
                                  const Ice::Current& current)
 {
-    ::std::string ns(s);
-    return ns.length();
+    //::std::string ns(s);
+    return 0;
 }
 
 ::std::string
 UC3M::StringServiceI::toUpperCase(const ::std::string& s,
                                   const Ice::Current& current)
 {
-  ::std::string ns(s);
-  ::std::transform(ns.begin(), ns.end(), ns.begin(), ::toupper);
-  return ns;
+  //::std::string ns(s);
+  //::std::transform(ns.begin(), ns.end(), ns.begin(), ::toupper);
+  return "ns";
 
 }
