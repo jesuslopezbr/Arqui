effb33321e0fd9db1aa6ab753122b5425c79a65f
100346250 <100346250@alumnos.uc3m.es>
2018-05-03 10:55:12 +0200

    Pract 4

---
+++ Practica4.2/comp.sh
@@ -0,0 +1,10 @@
+#bin/bash
+
+rm main controlclientes.o
+#Monitor Uni
+#gcc-4.9 -I. -c controlclientes.cpp
+#gcc-4.9 -o main controlclientes.o -lstdc++
+#PC
+gcc -c controlclientes.cpp
+gcc -o main controlclientes.o -lstdc++ -pthread
+./main 1
