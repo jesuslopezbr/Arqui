a366f54526419617e40b1bfc7b531445487facb9
GRADO EN INGENIERIA TELEMATICA <0346898@lab.it.uc3m.es>
2018-05-03 16:34:47 +0200

    interfaz

---
+++ Practica4.2/interfazI.cpp
@@ -0,0 +1,28 @@
+
+#include <interfazI.h>
+
+void
+Demo::interfazI::altaUsr(const Ice::Current& current)
+{
+}
+
+void
+Demo::interfazI::bajaUsr(const Ice::Current& current)
+{
+}
+
+void
+Demo::interfazI::actualizarDesc(::Ice::Int time,
+                                const Ice::Current& current)
+{
+}
+
+void
+Demo::interfazI::cambiarTarifa(const Ice::Current& current)
+{
+}
+
+void
+Demo::interfazI::terminar(const Ice::Current& current)
+{
+}
