0a73f144ec3baea9224125e2e2a517c488d66581
Jesús López <jesuslopez@MacBook-Pro-de-Jesus.local>
2018-05-03 20:07:09 +0200

    interfaz

--- Practica4.2/controlclientes.cpp
+++ Practica4.2/controlclientes.cpp
@@ -1,4 +1,34 @@
+#include <string>
+#include <pthread.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <iostream>
+#include <stdio.h>
 #include <controlclientes.h>
+
+using namespace std;
+
+#define MAX_CLIENTES 50
+
+pthread_t h_desc, h_factura;
+pthread_mutex_t clients_mutex;
+pthread_mutex_t loop_mutex;
+pthread_cond_t cambio_desc;
+
+int i = 0, clientes = 0, ex = 0;
+int loop = 1;
+
+struct info_cliente
+{
+  unsigned dni;
+  string nombre;
+  char tarifa;
+  unsigned alta;
+  unsigned descuento;
+};
+typedef struct info_cliente info_cliente_t;
+
+info_cliente_t datos_c1[MAX_CLIENTES];
 
 void menu()
 {
