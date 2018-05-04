139bb540aecfaa05d3c6024c78eee11cacb27862
100346250 <100346250@alumnos.uc3m.es>
2018-05-04 17:18:05 +0200

    merge y script

--- Practica4.2/comp.sh
+++ Practica4.2/comp.sh
@@ -1,14 +1,13 @@
-#bin/bash
+#!/bin/bash
 rm servidor.o interfazI.o controlclientes.o interfaz.o cliente.o
 
 
-if [ $1 -eq "servidor" ] then
-  Servidor:
+if [ $1 == "servidor" ]
+then
   gcc-4.9 -I. -c interfaz.cpp controlclientes.cpp servidor.cpp interfazI.cpp
   gcc-4.9 -o servidor servidor.o interfazI.o controlclientes.o interfaz.o -lIce -lIceUtil -pthread -lstdc++
   ./servidor
 else
-  Cliente:
   gcc-4.9 -I. -c interfaz.cpp controlclientes.cpp cliente.cpp interfazI.cpp
   gcc-4.9 -o cliente cliente.o interfazI.o controlclientes.o interfaz.o -lIce -lIceUtil -pthread -lstdc++
   ./cliente 2
