463d5ebd213331d648dbe96d6c6badc2ca889ff9
100346250 <100346250@alumnos.uc3m.es>
2018-05-04 18:44:01 +0200

    Test

--- Practica4.2/Test/testI.cpp
+++ Practica4.2/Test/testI.cpp
@@ -6,5 +6,7 @@ M::TestI::op(const ::std::string& sin,
              ::std::string& sout,
              const Ice::Current& current)
 {
+    cout << sin << endl;
+    sout = "Hello";
     return 0;
 }
