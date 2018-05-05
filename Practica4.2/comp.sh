a76dd23811f8dfb21cdfb885cb0deb7d853b1efc
100346250 <100346250@alumnos.uc3m.es>
2018-05-05 18:10:46 +0200

    Intento nuevo

--- Practica4.2/comp.sh
+++ Practica4.2/comp.sh
@@ -4,8 +4,8 @@ rm servidor.o interfazI.o controlclientes.o interfaz.o cliente.o servidor client
 
 if [ $1 == "servidor" ]
 then
-  gcc-4.9 -I. -c interfaz.cpp controlclientes.cpp servidor.cpp interfazI.cpp
-  gcc-4.9 -o servidor servidor.o interfazI.o controlclientes.o interfaz.o -lIce -lIceUtil -pthread -lstdc++
+  gcc-4.9 -std=c++11 -I. -c interfaz.cpp controlclientes.cpp servidor.cpp interfazI.cpp
+  gcc-4.9  -o servidor servidor.o interfazI.o controlclientes.o interfaz.o -lIce -lIceUtil -pthread -lstdc++
   ./servidor
 else
   gcc-4.9 -I. -c interfaz.cpp controlclientes.cpp cliente.cpp interfazI.cpp
