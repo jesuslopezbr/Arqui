db86df6c5a6d6701b41ab9a5655e70480fa6be89
100346250 <100346250@alumnos.uc3m.es>
2018-05-07 23:38:06 +0200

    Debug

--- Practica4.2/interfazI.cpp
+++ Practica4.2/interfazI.cpp
@@ -15,7 +15,7 @@ Demo::interfazI::checkUsr(const ::std::string& dni,
   cout << endl << dni << endl;
   unsigned dni2 = stoul(dni,nullptr,0);
   cout << dni2 << endl;
-  dni = NULL;
+  dni = "";
   sout = check_usr(dni2,opcion);
 }
 
